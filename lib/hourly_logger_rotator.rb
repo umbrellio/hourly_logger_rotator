a8a2d1a1d82c11a97f0fe04358a189df7bc50209
Dmitry Gubitskiy <d.gubitskiy@gmail.com>
2018-03-06 14:21:11 +0300

    Can set default rotation period

--- lib/hourly_logger_rotator.rb
+++ lib/hourly_logger_rotator.rb
@@ -3,16 +3,48 @@
 require "logger"
 require "hourly_logger_rotator/version"
 
-Dir[File.join(File.dirname(__FILE__), "hourly_logger_rotator", "patch", "*.rb")].each do |file|
-  require_relative(file)
+pattern = File.join(File.dirname(__FILE__), "hourly_logger_rotator", "patch", "*.rb")
+Dir[pattern].each { |file| require_relative(file) }
+
+module HourlyLoggerRotator
+  class << self
+    def add_hourly_rotation_period_support!
+      if hourly_mixin
+        Logger::LogDevice.prepend(hourly_mixin)
+      else
+        warn unsupported_warning
+      end
+    end
+
+    def default_rotation_period=(period)
+      raise unsupported_warning unless hourly_mixin
+
+      mixin = Module.new do
+        define_method(:initialize) do |logdev, shift_age = period, *args|
+          super(logdev, shift_age, *args)
+        end
+      end
+
+      Logger.prepend(mixin)
+    end
+
+    private
+
+    def hourly_mixin
+      return @mixin if defined?(@mixin)
+
+      mixin_name = "HourlyLoggerRotator::Patch::Ruby_#{RUBY_VERSION.split('.').first(2).join('_')}"
+      @mixin = begin
+        Module.const_get(mixin_name)
+      rescue NameError
+        nil
+      end
+    end
+
+    def unsupported_warning
+      "Hourly log rotation period is not supported for Ruby #{RUBY_VERSION}"
+    end
+  end
 end
 
-mixin_name = "HourlyLoggerRotator::Patch::Ruby_#{RUBY_VERSION.split('.').first(2).join('_')}"
-
-mixin = begin
-  Module.const_get(mixin_name)
-rescue NameError
-  nil
-end
-
-Logger::LogDevice.prepend(mixin) if mixin
+HourlyLoggerRotator.add_hourly_rotation_period_support!
